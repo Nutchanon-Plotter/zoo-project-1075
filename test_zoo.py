c81deff5ef1f0d8c9a65e678cf5330b2025925b1
Nutchanon Boonyato <65450526+Nutchanon-Plotter@users.noreply.github.com>
2024-11-14 08:26:17 +0000

    finish

--- test_zoo.py
+++ test_zoo.py
@@ -6,8 +6,18 @@ class TestZoo(unittest.TestCase):
         self.zoo = Zoo()
 
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+
+    def test_teenage_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+    def test_middle_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+
+    def test_old_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(70), 100)
+      
+      
     # Add your additional test cases here.
 
 if __name__ == '__main__':
